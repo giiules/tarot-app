8e2641f6a03fa801926a1c5b835af8c937887479
Giu <giuliaserrelli@MacBook-Pro-di-Giu.local>
2025-06-13 19:03:59 +0100

    Fix: use DB_PORT to avoid conflict with Railway PORT

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let config = {};
 config.sql = {};
 
 config.sql.host = process.env.HOST;
-config.sql.port = process.env.PORT;
+config.sql.port = process.env.DB_PORT;
 config.sql.user = process.env.USER;
 config.sql.password = process.env.PASSWORD;
 config.sql.database = process.env.DATABASE;
