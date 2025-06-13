bc112c2d67211b3cb6f15acbe599c4c1732b93c6
Giu <giuliaserrelli@MacBook-Pro-di-Giu.local>
2025-06-13 19:30:32 +0100

    updated env

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let config = {};
 config.sql = {};
 
-config.sql.host = process.env.MYSQLHOST;
-config.sql.port = process.env.MYSQLPORT;
-config.sql.user = process.env.MYSQLUSER;
-config.sql.password = process.env.MYSQLPASSWORD;
-config.sql.database = process.env.MYSQLDATABASE;
+config.sql.host = process.env.HOST; 
+config.sql.port = process.env.PORT;
+config.sql.user = process.env.USER;
+config.sql.password = process.env.PASSWORD;
+config.sql.database = process.env.DATABASE;
 
 module.exports = config;
